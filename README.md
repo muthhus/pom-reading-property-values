ebd714a6caf6b8a4a32e8d0e12a490b944d3262e
Rithicaa <rithicaavm@telekomatrix.com>
2019-06-06 06:48:25 +0100

    Init with pom is reading values from prop implementation

---
+++ README.md
@@ -0,0 +1,1 @@
+# pom-reading-property-values
---
+++ src/test/java/com/telekomatrix/support/community/PomReaderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.telekomatrix.support.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PomReaderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
